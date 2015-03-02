9931bb3f31573edcca827913ba74d44d67a19f59
Heather Anderson and Kameron Johnson <heather@thisisnotanorg.org>
2015-03-02 14:52:26 -0800

    add store.js and corrected routes

---
+++ store.js
@@ -0,0 +1,26 @@
+var posts = [
+  {
+    id: '1',
+    title: "Cary Elwes becomes a real prince!",
+    body: "For real!",
+    image: "img/cary.gif"
+  },
+  {
+    id: '2',
+    title: "Justin Bieber dies on his 21st Birthday!",
+    body: "Yep, Justin Bieber Died.",
+    image: "img/bieber.gif"
+  },
+  {
+    id: '3',
+    title: "Bret Easton Ellis killed by Patrick Bateman! Gory details inside!",
+    body: "While listening to Huey Lewis and the News",
+    image: "img/patrick.gif"
+  },
+  {
+    id: '4',
+    title: "Michelle Pfeiffer turns into a real cat! Eats corpse of Bret Eason Ellis!",
+    body: "And gets Rabies!",
+    image: "img/michelle.gif"
+  }
+]
