7bf95d693b2883a1b83d49064f94df240b0cfbb1
Heather Anderson and Kameron Johnson <KameronJohnson@gmail.com>
2015-03-02 16:50:45 -0800

    add nav categories and clean up

--- router.js
+++ router.js
@@ -1,4 +1,6 @@
 Bustle.Router.map(function() {
   this.resource('posts', {path: '/'});
   this.resource('post', {path: 'posts/:post_id'});
+  this.resource('about');
+  this.resource('contributors');
 });
